cc6631c9525d3b5c7b09ca6a017a97c698fba79e
drishti agrawal <drishti29agrawal@gmail.com>
2023-03-15 16:37:09 +0530

    Your orders page added

--- lib/components/widgets.dart
+++ lib/components/widgets.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:pizza_planet/utils.dart';
 
-Widget categoryItem(String path){
+Widget categoryItem(String path) {
   return Container(
     margin: const EdgeInsets.fromLTRB(15, 18, 15, 5),
     padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
     decoration: BoxDecoration(
       color: Colors.white,
-      border: Border.all(color: primaryBorderDarkWhite,width: 0.4),
+      border: Border.all(color: primaryBorderDarkWhite, width: 0.4),
       borderRadius: BorderRadius.circular(10),
       boxShadow: const [
         BoxShadow(
-          color: Color(0xFFB9B9B9),
-          blurRadius: 6,
-          offset: Offset(
-          0,
-          6,
-          )
-        )
+            color: Color(0xFFB9B9B9),
+            blurRadius: 6,
+            offset: Offset(
+              0,
+              6,
+            ))
       ],
     ),
     child: Image(
