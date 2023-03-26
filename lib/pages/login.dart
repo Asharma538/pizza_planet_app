431beff22da1de4372041e07f533c03c327b5dab
Anadi Sharma <sharma.130@iitj.ac.in>
2023-03-26 14:47:23 +0530

    Otp Verification feature added

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,163 +1,141 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
-import 'package:pizza_planet/main_screen.dart';
+import 'package:pizza_planet/pages/otp.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
-
+  static String verify = "";
   @override
   State<Login> createState() => _LoginState();
 }
 
 class _LoginState extends State<Login> {
+  var pn = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Container(
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.fromLTRB(0, 0, 0, MediaQuery.of(context).size.height*0.04),
-              child: Image(
-                image: const AssetImage('images/main_page_pizza.jpg'),
-                height: MediaQuery.of(context).size.height * 0.5,
-                fit: BoxFit.cover,
-              ),
-              // margin: MediaQuery.of(context).size.height*0.08,
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 0, 0, MediaQuery.of(context).size.height*0.04),
+            child: Image(
+              image: const AssetImage('images/main_page_pizza.jpg'),
+              height: MediaQuery.of(context).size.height * 0.5,
+              fit: BoxFit.cover,
             ),
-            const Text('Find your favourite food',
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-            const Padding(padding: EdgeInsets.only(bottom: 16)),
-            Row(
-              children: <Widget>[
-                Expanded(
-                    child: Container(
-                  margin: EdgeInsets.only(left: 25.0, right: 15.0),
-                  child: const Divider(
-                    color: Color.fromARGB(255, 146, 146, 146),
-                    height: 50,
-                  ),
-                )),
-                const Text(
-                  'Login / Sign Up',
-                  style: TextStyle(fontSize: 16),
-                ),
-                Expanded(
-                    child: Container(
-                      margin: const EdgeInsets.only(left: 15.0, right: 25.0),
-                      child: const Divider(
-                        color: Color.fromARGB(255, 146, 146, 146),
-                        height: 50,
-                  ),
-                ))
-              ],
-            ),
-            Container(
-              alignment: Alignment.center,
-              height: 62,
-              width: 290,
-              child: IntlPhoneField(
-                textAlignVertical: TextAlignVertical.center,
-                decoration: const InputDecoration(
-                    // floatingLabelAlignment: FloatingLabelAlignment.center,
-                    labelText: 'Mobile Number',
-                    labelStyle: TextStyle(fontSize: 15),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(),
-                    )),
-                initialCountryCode: 'IN',
-                onChanged: (phone) {
-                  // print(phone.completeNumber);
-                },
-              ),
-            ),
-            const Padding(padding: EdgeInsets.only(bottom: 10)),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const MainScreen()),
-                );
-              },
-              style: ButtonStyle(
-                  fixedSize: MaterialStateProperty.all(const Size(290, 40)),
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.white),
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                          side: BorderSide(color: Colors.red)))),
-              child: const Text(
-                'Continue',
-                style: TextStyle(fontSize: 15),
-              ),
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                    child: Container(
+            // margin: MediaQuery.of(context).size.height*0.08,
+          ),
+          const Text('Find your favourite food',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+          const Padding(padding: EdgeInsets.only(bottom: 16)),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
                   margin: const EdgeInsets.only(left: 25.0, right: 15.0),
                   child: const Divider(
-                    color: Color.fromARGB(255, 146, 146, 146),
-                    height: 50,
-                  ),
-                )),
-                const Text('Or'),
-                Expanded(
-                    child: Container(
-                  margin: const EdgeInsets.only(left: 15.0, right: 25.0),
-                  child: const Divider(
-                    color: Color.fromARGB(255, 146, 146, 146),
-                    height: 50,
-                  ),
-                )),
-              ],
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey.shade400),
-                  borderRadius: BorderRadius.circular(5),
-                  ),
-              child: const Image(
-                image: AssetImage('images/google_sign_in2.png'),
-                width: 290,
-                height: 50,
+                  color: Color.fromARGB(255, 146, 146, 146),
+                  height: 50,
+                ),
+              )),
+              const Text(
+                'Login / Sign Up',
+                style: TextStyle(fontSize: 16),
               ),
-
-            )
-
-            // SizedBox(
-
-            //   width: 290,
-            // child: Container(
-            //   height: 40,
-
-            //   decoration: BoxDecoration(
-            //     border: Border.all(color: Colors.grey.shade300 ),
-            //     borderRadius: BorderRadius.all(Radius.circular(8)),
-
-            //   ),
-            //   child: SignInButton(
-            //     Buttons.Google,
-            //     onPressed: (){},
-            //     text: "Sign up with Google",
-
-            //     )
-            //   ),
-            // ),
-
-            // Row(
-            //   children: [
-            //     Image(image: AssetImage('images/google_sign_in.png'),
-            //     width: 290,
-            //     height: 40,
-            //     )
-            //   ],
-            // )
-          ],
-        ),
+              Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 15.0, right: 25.0),
+                    child: const Divider(
+                      color: Color.fromARGB(255, 146, 146, 146),
+                      height: 50,
+                ),
+              ))
+            ],
+          ),
+          Container(
+            alignment: Alignment.center,
+            height: 62,
+            width: 290,
+            child: IntlPhoneField(
+              textAlignVertical: TextAlignVertical.center,
+              decoration: const InputDecoration(
+                  labelText: 'Mobile Number',
+                  labelStyle: TextStyle(fontSize: 15),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(),
+                  )),
+              initialCountryCode: 'IN',
+              onChanged: (phone) {
+                pn = (phone.completeNumber).toString();
+              },
+            ),
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 10)),
+          ElevatedButton(
+            onPressed: () async{
+              await FirebaseAuth.instance.verifyPhoneNumber(
+                phoneNumber: pn,
+                verificationCompleted: (PhoneAuthCredential credential) {},
+                verificationFailed: (FirebaseAuthException e) {},
+                codeSent: (String verificationId, int? resendToken) {
+                  Login.verify =  verificationId;
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Otp()),
+                  );
+                },
+                codeAutoRetrievalTimeout: (String verificationId) {},
+              );
+            },
+            style: ButtonStyle(
+                fixedSize: MaterialStateProperty.all(const Size(290, 40)),
+                foregroundColor:
+                    MaterialStateProperty.all<Color>(Colors.white),
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        side: BorderSide(color: Colors.red)))),
+            child: const Text(
+              'Continue',
+              style: TextStyle(fontSize: 15),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 25.0, right: 15.0),
+                    child: const Divider(
+                      color: Color.fromARGB(255, 146, 146, 146),
+                      height: 50,
+                    ),
+              )),
+              const Text('Or'),
+              Expanded(
+                  child: Container(
+                margin: const EdgeInsets.only(left: 15.0, right: 25.0),
+                child: const Divider(
+                  color: Color.fromARGB(255, 146, 146, 146),
+                  height: 50,
+                ),
+              )),
+            ],
+          ),
+          Container(
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey.shade400),
+                borderRadius: BorderRadius.circular(5),
+                ),
+            child: const Image(
+              image: AssetImage('images/google_sign_in2.png'),
+              width: 290,
+              height: 50,
+            ),
+          )
+        ],
       ),
     );
   }
---
+++ lib/pages/otp.dart
@@ -0,0 +1,151 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:pizza_planet/main_screen.dart';
+import 'package:pizza_planet/utils.dart';
+import 'package:pinput/pinput.dart';
+
+import 'login.dart';
+
+class Otp extends StatefulWidget {
+  const Otp({Key? key}) : super(key: key);
+
+  @override
+  State<Otp> createState() => _OtpState();
+}
+
+class _OtpState extends State<Otp> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  @override
+  Widget build(BuildContext context) {
+    final defaultPinTheme = PinTheme(
+      width: 56,
+      height: 56,
+      textStyle: const TextStyle(fontSize: 20, color: Color.fromRGBO(30, 60, 87, 1), fontWeight: FontWeight.w600),
+      decoration: BoxDecoration(
+        border: Border.all(color: const Color.fromRGBO(234, 239, 243, 1)),
+        borderRadius: BorderRadius.circular(20),
+      ),
+    );
+
+    final focusedPinTheme = defaultPinTheme.copyDecorationWith(
+      border: Border.all(color: const Color.fromRGBO(114, 178, 238, 1)),
+      borderRadius: BorderRadius.circular(8),
+    );
+
+    final submittedPinTheme = defaultPinTheme.copyWith(
+      decoration: defaultPinTheme.decoration?.copyWith(
+        color: const Color.fromRGBO(234, 239, 243, 1),
+      ),
+    );
+    var otp_code="";
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 50,),
+            const Image(image: AssetImage('images/otpImg.png')),
+            const Text('Find your favourite food',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+            const Padding(padding: EdgeInsets.only(bottom: 16)),
+            Row(
+              children: <Widget>[
+                Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 25.0, right: 15.0),
+                      child: const Divider(
+                        color: Color.fromARGB(255, 146, 146, 146),
+                        height: 50,
+                      ),
+                    )),
+                const Text(
+                  'Enter the verification code',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 15.0, right: 25.0),
+                      child: const Divider(
+                        color: Color.fromARGB(255, 146, 146, 146),
+                        height: 50,
+                      ),
+                    ))
+              ],
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 62,
+              width: 290,
+              child: Pinput(
+                defaultPinTheme: PinTheme(
+                  width: 56,
+                  height: 50,
+                  textStyle: const TextStyle(fontSize: 20, color: Color.fromRGBO(30, 60, 87, 1), fontWeight: FontWeight.w600),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: const Color(0xFFB4B4B4),width: 1),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                length: 6,
+                androidSmsAutofillMethod: AndroidSmsAutofillMethod.smsUserConsentApi,
+                autofocus: true,
+                pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
+                showCursor: true,
+                // onCompleted: (pin){},
+                onChanged: (value) {
+                  otp_code = value;
+                },
+              ),
+            ),
+            const SizedBox(height:10),
+            ElevatedButton(
+              onPressed: () async{
+                try {
+                  PhoneAuthCredential credential = PhoneAuthProvider.credential(
+                      verificationId: Login.verify, smsCode: otp_code);
+                  // Sign the user in (or link) with the credential
+                  await auth.signInWithCredential(credential);
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MainScreen()),
+                  );
+                }
+                catch (e){
+                  print('wrong otp');
+                }
+              },
+              style: ButtonStyle(
+                  fixedSize: MaterialStateProperty.all(const Size(290, 40)),
+                  foregroundColor:
+                  MaterialStateProperty.all<Color>(Colors.white),
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          side: const BorderSide(color: Colors.red)))),
+              child: const Text(
+                'Continue',
+                style: TextStyle(fontSize: 15),
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                    margin: const EdgeInsets.only(left: 25.0, top: 20.0),
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => const Login()),
+                        );
+                      },
+                      child: const Text("Edit your number",style: TextStyle(color: Colors.black),),)
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
